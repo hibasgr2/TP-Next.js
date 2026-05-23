55815255e8722000a0c2c96c90ec2f80d0a7cd5c
Hiba-saghir <saghirheba2@gmail.com>
2026-05-23 14:07:24 +0100

    Deploiment

--- TaskFlow_Next-/lib/prisma.ts
+++ TaskFlow_Next-/lib/prisma.ts
@@ -1,19 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// import { PrismaClient } from "@prisma/client";
-
-// export const prisma = new PrismaClient();
-
-// import { PrismaClient } from "@prisma/client";
-
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
